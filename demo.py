e3d8921e019fbdd611b3a8d9f4392a54f735b58b
Emmanuel Owusu Ahenkan <56390806+EmmanuelOwusu@users.noreply.github.com>
2021-03-12 07:26:12 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -84,73 +84,73 @@ if __name__ == '__main__':
 
 
 
-    """ DEMO 3 WITH TRANSFORMS """
-    # As of torchvision 0.8.0, torchvision transforms support batches of images
-    # of size (BATCH x CHANNELS x HEIGHT x WIDTH) and apply deterministic or random
-    # transformations on the batch identically on all images of the batch. Any torchvision
-    # transform for image augmentation can thus also be used  for video augmentation.
-    preprocess = transforms.Compose([
-        ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
-        transforms.Resize(299),  # image batch, resize smaller edge to 299
-        transforms.CenterCrop(299),  # image batch, center crop to square 299x299
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+#     """ DEMO 3 WITH TRANSFORMS """
+#     # As of torchvision 0.8.0, torchvision transforms support batches of images
+#     # of size (BATCH x CHANNELS x HEIGHT x WIDTH) and apply deterministic or random
+#     # transformations on the batch identically on all images of the batch. Any torchvision
+#     # transform for image augmentation can thus also be used  for video augmentation.
+#     preprocess = transforms.Compose([
+#         ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
+#         transforms.Resize(299),  # image batch, resize smaller edge to 299
+#         transforms.CenterCrop(299),  # image batch, center crop to square 299x299
+#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+#     ])
 
-    dataset = VideoFrameDataset(
-        root_path=videos_root,
-        annotationfile_path=annotation_file,
-        num_segments=5,
-        frames_per_segment=1,
-        imagefile_template='img_{:05d}.jpg',
-        transform=preprocess,
-        random_shift=True,
-        test_mode=False
-    )
+#     dataset = VideoFrameDataset(
+#         root_path=videos_root,
+#         annotationfile_path=annotation_file,
+#         num_segments=5,
+#         frames_per_segment=1,
+#         imagefile_template='img_{:05d}.jpg',
+#         transform=preprocess,
+#         random_shift=True,
+#         test_mode=False
+#     )
 
-    sample = dataset[2]
-    frame_tensor = sample[0]  # tensor of shape (NUM_SEGMENTS*FRAMES_PER_SEGMENT) x CHANNELS x HEIGHT x WIDTH
-    label = sample[1]  # integer label
+#     sample = dataset[2]
+#     frame_tensor = sample[0]  # tensor of shape (NUM_SEGMENTS*FRAMES_PER_SEGMENT) x CHANNELS x HEIGHT x WIDTH
+#     label = sample[1]  # integer label
 
-    print('Video Tensor Size:', frame_tensor.size())
+#     print('Video Tensor Size:', frame_tensor.size())
 
-    def denormalize(video_tensor):
-        """
-        Undoes mean/standard deviation normalization, zero to one scaling,
-        and channel rearrangement for a batch of images.
-        args:
-            video_tensor: a (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
-        """
-        inverse_normalize = transforms.Normalize(
-            mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.225],
-            std=[1 / 0.229, 1 / 0.224, 1 / 0.225]
-        )
-        return (inverse_normalize(video_tensor) * 255.).type(torch.uint8).permute(0, 2, 3, 1).numpy()
-
-
-    frame_tensor = denormalize(frame_tensor)
-    plot_video(rows=1, cols=5, frame_list=frames, plot_width=15., plot_height=3.)
+#     def denormalize(video_tensor):
+#         """
+#         Undoes mean/standard deviation normalization, zero to one scaling,
+#         and channel rearrangement for a batch of images.
+#         args:
+#             video_tensor: a (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
+#         """
+#         inverse_normalize = transforms.Normalize(
+#             mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.225],
+#             std=[1 / 0.229, 1 / 0.224, 1 / 0.225]
+#         )
+#         return (inverse_normalize(video_tensor) * 255.).type(torch.uint8).permute(0, 2, 3, 1).numpy()
 
 
+#     frame_tensor = denormalize(frame_tensor)
+#     plot_video(rows=1, cols=5, frame_list=frames, plot_width=15., plot_height=3.)
 
-    """ DEMO 3 CONTINUED: DATALOADER """
-    dataloader = torch.utils.data.DataLoader(
-        dataset=dataset,
-        batch_size=2,
-        shuffle=True,
-        num_workers=4,
-        pin_memory=True
-    )
 
-    for epoch in range(10):
-        for video_batch, labels in dataloader:
-            """
-            Insert Training Code Here
-            """
-            print(labels)
-            print("\nVideo Batch Tensor Size:", video_batch.size())
-            print("Batch Labels Size:", labels.size())
-            break
-        break
+
+#     """ DEMO 3 CONTINUED: DATALOADER """
+#     dataloader = torch.utils.data.DataLoader(
+#         dataset=dataset,
+#         batch_size=2,
+#         shuffle=True,
+#         num_workers=4,
+#         pin_memory=True
+#     )
+
+#     for epoch in range(10):
+#         for video_batch, labels in dataloader:
+#             """
+#             Insert Training Code Here
+#             """
+#             print(labels)
+#             print("\nVideo Batch Tensor Size:", video_batch.size())
+#             print("Batch Labels Size:", labels.size())
+#             break
+#         break
 
 
 #     """ DEMO 5: SAMPLES WITH MULTIPLE LABELS """
